b5cdf9fca35327d6e8df22fc04eb0a9c676a48e3
gitbyul <gitbyul@gmail.com>
2026-03-31 16:22:31 +0900

    init

---
+++ src/main/java/com/gitbyul/sso_platform/SsoPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gitbyul.sso_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitbyul/sso_platform/SsoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitbyul.sso_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
